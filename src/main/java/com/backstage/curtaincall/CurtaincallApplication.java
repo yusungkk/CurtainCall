662fbebeddde5e58caeccfdcd1ed0712d438a627
KeeYong <ki425125@gmail.com>
2025-02-05 13:47:04 +0900

    init

---
+++ src/main/java/com/backstage/curtaincall/CurtaincallApplication.java
@@ -0,0 +1,13 @@
+package com.backstage.curtaincall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurtaincallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurtaincallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backstage/curtaincall/CurtaincallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backstage.curtaincall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurtaincallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
