99e00be1690ba41f8ed05d30607bbb557aa5ab38
Ellie <2unmi25@naver.com>
2025-02-13 16:27:56 +0900

    feat: jwt 검증

--- src/main/java/com/backstage/curtaincall/user/repository/UserRepository.java
+++ src/main/java/com/backstage/curtaincall/user/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.backstage.curtaincall.user.repository;
 
-import com.backstage.curtaincall.user.entity.ProviderType;
 import com.backstage.curtaincall.user.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
