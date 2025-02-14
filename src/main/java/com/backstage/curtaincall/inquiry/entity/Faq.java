c171674a6e4b3b8cf56cadcfe462724673ec38d9
KeeYong <ki425125@gmail.com>
2025-02-14 14:46:23 +0900

    refactor: 클래스명 FaqType -> QuestionType 변경

--- src/main/java/com/backstage/curtaincall/inquiry/entity/Faq.java
+++ src/main/java/com/backstage/curtaincall/inquiry/entity/Faq.java
@@ -20,22 +20,22 @@ public class Faq extends BaseEntity {
     private String question;
 
     @Enumerated(EnumType.STRING)
-    private FaqType type;
+    private QuestionType type;
 
 
-    public Faq(String answer, String question, FaqType type) {
+    public Faq(String answer, String question, QuestionType type) {
         this.answer = answer;
         this.question = question;
         this.type = type;
     }
 
-    public static Faq create(String answer, String question, FaqType type) {
+    public static Faq create(String answer, String question, QuestionType type) {
         return new Faq(answer, question, type);
     }
 
-    public void updateFaq(String answer, String question, FaqType faqType) {
+    public void updateFaq(String answer, String question, QuestionType questionType) {
         this.answer = answer;
         this.question = question;
-        this.type = faqType;
+        this.type = questionType;
     }
 }
