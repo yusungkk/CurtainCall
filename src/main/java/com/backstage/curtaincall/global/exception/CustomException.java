b6151c1723aa695e649b5ad04df7d0b18ce09151
박희재 <=>
2025-02-19 14:03:59 +0900

    feat- CustomException

--- src/main/java/com/backstage/curtaincall/global/exception/CustomException.java
+++ src/main/java/com/backstage/curtaincall/global/exception/CustomException.java
@@ -1,10 +1,13 @@
 package com.backstage.curtaincall.global.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class CustomException extends RuntimeException {
     private CustomErrorCode customErrorCode;
+
+    public CustomException(CustomErrorCode customErrorCode) {
+        super(customErrorCode.getMessage());  // 메시지를 상위 예외에 전달
+        this.customErrorCode = customErrorCode;
+    }
 }
